6f6efea056e9aba32ab141c2fc9eebfd16e44ac2
JustinBeaudry <me@justinbeaudry.dev>
2026-01-16 12:32:50 -0500

    Add missing has_many :projects association to Account model
    Fixes 500 error on Projects page due to undefined method 'projects' for Account.

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,7 @@ class Account < ApplicationRecord
   has_one :join_code
   has_many :users, dependent: :destroy
   has_many :boards, dependent: :destroy
+  has_many :projects, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :webhooks, dependent: :destroy
   has_many :tags, dependent: :destroy
